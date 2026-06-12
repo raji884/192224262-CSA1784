2ec3dc60359241c91874f5e583065e78ed82f12e
raji884 <nukalarajeswari4262.sse@saveetha.com>
2026-06-12 09:06:47 +0530

    Add files via upload

---
+++ monkey banana.pl
@@ -0,0 +1,7 @@
+% Monkey can get banana if it climbs on the box
+
+can_get_banana :-
+    write('Monkey goes to the box'), nl,
+    write('Monkey pushes the box under the banana'), nl,
+    write('Monkey climbs on the box'), nl,
+    write('Monkey takes the banana'), nl.

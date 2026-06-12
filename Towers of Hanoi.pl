5108dbb9248a2ed1508b3955458c0f8a6d3dee6d
raji884 <nukalarajeswari4262.sse@saveetha.com>
2026-06-12 08:58:56 +0530

    Add files via upload

---
+++ Towers of Hanoi.pl
@@ -0,0 +1,19 @@
+% Base case: Move one disk
+hanoi(1, Source, Destination, _) :-
+    write('Move disk 1 from '),
+    write(Source),
+    write(' to '),
+    write(Destination), nl.
+
+% Recursive case
+hanoi(N, Source, Destination, Auxiliary) :-
+    N > 1,
+    M is N - 1,
+    hanoi(M, Source, Auxiliary, Destination),
+    write('Move disk '),
+    write(N),
+    write(' from '),
+    write(Source),
+    write(' to '),
+    write(Destination), nl,
+    hanoi(M, Auxiliary, Destination, Source).

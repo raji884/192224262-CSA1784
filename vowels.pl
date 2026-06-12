2e730896b169d492ee048f25851a5b9c5d6e8717
raji884 <nukalarajeswari4262.sse@saveetha.com>
2026-06-12 09:47:49 +0530

    Add files via upload

---
+++ vowels.pl
@@ -0,0 +1,18 @@
+% Check vowel
+vowel(a).
+vowel(e).
+vowel(i).
+vowel(o).
+vowel(u).
+
+% Count vowels in a list
+count_vowels([], 0).
+
+count_vowels([H|T], N) :-
+    vowel(H),
+    count_vowels(T, N1),
+    N is N1 + 1.
+
+count_vowels([H|T], N) :-
+    \+ vowel(H),
+    count_vowels(T, N).
